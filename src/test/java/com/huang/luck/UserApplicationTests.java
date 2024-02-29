269f6cdf8d63147410e485ce0e3e14d81191be17
Huang <Huang@test.com>
2024-02-29 11:00:55 +0800

    目前是大概写完了redis接口，但是现在的问题是mapper层怎么弄两个，好像会出现冲突，解决了这个问题就大概全部完成了

--- src/test/java/com/huang/luck/UserApplicationTests.java
+++ src/test/java/com/huang/luck/UserApplicationTests.java
@@ -2,17 +2,22 @@ package com.huang.luck;
 
 import com.huang.luck.controller.UserController;
 import com.huang.luck.entity.User;
+import com.huang.luck.mapper.AdminMapper;
 import com.huang.luck.mapper.UserMapper;
 import com.huang.luck.service.UserService;
+import com.huang.luck.service.UserServiceImpl.UserServiceImplRedis;
 import com.huang.luck.util.LuckTool.Md5Encryption;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -25,6 +30,8 @@ public class UserApplicationTests {
     UserService userService;
     @Autowired
     UserController userController;
+    @Autowired
+    UserServiceImplRedis userServiceImplRedis;
     @Test
    public void UserRegMapperInsert(){   //有用 11.11
         User user = new User();
@@ -68,4 +75,50 @@ public class UserApplicationTests {
         jedis.set("k3","123");
         System.out.println(jedis.get("k3"));
     }
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+   @Test
+    public void RedisZset(){
+       redisTemplate.opsForSet().add("mySet",165);
+       for(int i=0;i<10;i++){
+           redisTemplate.opsForSet().add("mySet",i);
+       }
+
+       redisTemplate.opsForSet().add("mySet",16);
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+       System.out.println(redisTemplate.opsForSet().randomMembers("mySet",3));
+       System.out.println(   redisTemplate.opsForSet().pop("mySet"));
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+       System.out.println(   redisTemplate.opsForSet().pop("mySet"));
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+       System.out.println(redisTemplate.opsForSet().pop("mySet", 2));
+       System.out.println(redisTemplate.opsForSet().members("mySet"));
+
+
+   }
+    @Test
+    public  void a(){
+     /*   ArrayList<Object> objects = new ArrayList<Object>();
+       objects.add(redisTemplate.opsForSet().members("mySet"));
+        System.out.println(objects);*/
+       userServiceImplRedis.AdminRedisSet("test",40);
+       //这里是输出用户及拿到的卡片数字
+        userServiceImplRedis.UserGetCard("test","13197916220",20);
+        //这里是输出还剩余的卡片数字
+        System.out.println(redisTemplate.opsForSet().members("test"));
+    }
+
+
+
+    //无法自动装配   ？？？？
+    @Autowired
+    AdminMapper adminMapper;
+   @Test
+    public  void AdminCheckGoodsName(){
+       adminMapper.AdminGoodsNameCheck("迈阿密");
+    }
+
 }
