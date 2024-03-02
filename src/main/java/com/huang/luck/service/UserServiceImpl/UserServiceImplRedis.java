5471c7c7f54b275689d509f06fc19340bfe618c8
Huang <Huang@test.com>
2024-03-02 12:20:15 +0800

    修改了一下，把redistemplate改成了stringredistemplate，现在redis和springboot数据一致，就是抢卡的时候可能会出一些操作上的问题如：越界，

--- src/main/java/com/huang/luck/service/UserServiceImpl/UserServiceImplRedis.java
+++ src/main/java/com/huang/luck/service/UserServiceImpl/UserServiceImplRedis.java
@@ -2,12 +2,17 @@ package com.huang.luck.service.UserServiceImpl;
 
 import com.huang.luck.mapper.AdminMapper;
 import com.huang.luck.service.UserServiceRedis;
+import com.huang.luck.service.ex.CardNameException;
+import com.huang.luck.service.ex.CardNumberIsNull;
 import com.huang.luck.service.ex.GoodsListNULLException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.naming.Name;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +21,7 @@ import java.util.Map;
 @Component
 public class UserServiceImplRedis implements UserServiceRedis {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
     @Autowired
     AdminMapper adminMapper;
     //rediszset
@@ -26,21 +31,35 @@ public class UserServiceImplRedis implements UserServiceRedis {
     //此方法是管理员去调用生成一个set结构  生成的名字就利用mysql中武器名字
     public void AdminRedisSet(String name,int price){
         for(int i=0;i<price;i++){
-            redisTemplate.opsForSet().add(name,i);
+
+            //stringredistemplate的add方法只能添加string类型
+            stringRedisTemplate.opsForSet().add(name,String.valueOf(i));
         }
     }
 
     public void UserGetCard(String name,String userAccount,int price){
         //利用传递过来的名字和价格去抢卡片数字
         //版本1.0  名字还没有用上
-      if (name.equals(adminMapper.AdminGoodsNameCheck(name))) {
-          Map<String, ArrayList<Object>> Map = new HashMap<String, ArrayList<Object>>();
-          ArrayList<Object> objects = new ArrayList<Object>();
-          objects.add(redisTemplate.opsForSet().pop(name, price));
-          Map.put(userAccount, objects);
-          System.out.println(Map);
-      }
-      else{throw new  GoodsListNULLException("暂时还未收录这把武器");}
+         // String Name=name;
+          if (name.equals(null)) {
+              throw new GoodsListNULLException("出错了");
+    }
+          else if (stringRedisTemplate.opsForSet().size("Name")==0) {
+                if (name.equals(adminMapper.AdminGoodsNameCheck(name))) {
+                Map<String, ArrayList<Object>> Map = new HashMap<String, ArrayList<Object>>();
+                ArrayList<Object> objects = new ArrayList<Object>();
+                objects.add(stringRedisTemplate.opsForSet().pop(name, price));
+                Map.put(userAccount, objects);
+               // return Map;
+               System.out.println(Map);
+            }
+            //     throw  new
+         throw new CardNumberIsNull("没有卡片可以抢了");
+       }
+
+
+             else{throw new  GoodsListNULLException("暂时还未收录这把武器");}
+
     }
 
 }
